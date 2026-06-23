3fdaacf1f537e99f08812fc3731db016644754f7
santiagonieto09 <santiago.nieto09@gmail.com>
2026-06-23 15:11:34 -0500

    fix: eliminar fallbacks rotos de whatsapp://send y priorizar MethodChannel nativo

--- lib/data/services/share_service.dart
+++ lib/data/services/share_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:statuses/data/models/status_file.dart';
 import 'package:statuses/utils/file_utils.dart';
 
@@ -17,20 +16,7 @@ class ShareService {
       if (success == true) return;
     } catch (_) {}
 
-    try {
-      final whatsappUri = Uri.parse('whatsapp://send');
-      if (await canLaunchUrl(whatsappUri)) {
-        await launchUrl(whatsappUri);
-        return;
-      }
-    } catch (_) {}
-
-    try {
-      final waStatusUri = Uri.parse('https://wa.me/status');
-      await launchUrl(waStatusUri, mode: LaunchMode.externalApplication);
-    } catch (_) {
-      await _shareViaIntent(status);
-    }
+    await _shareViaIntent(status);
   }
 
   Future<void> _shareViaIntent(StatusFile status) async {
